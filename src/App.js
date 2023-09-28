5eb6c6d07b162cea37b715669e58bd82155e51b4
Benjtalkshow <chinedubenj@gmail.com>
2023-09-28 12:51:19 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     Hello World
+    <div className="App w-full">
+      <button className='text-white bg-yellow-500 px-10 py-2 rounded-lg mt-10 mb-10'>Click</button>
     </div>
   );
 }
